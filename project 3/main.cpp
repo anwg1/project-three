6b49669eb53788af0627034164c512ed86148f55
AF <angelinw@umich.edu>
2022-10-12 14:21:47 -0400

    Initial Commit

---
+++ project 3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project 3
+//
+//  Created by Angelina Wong on 10/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
